095d64c626547686d918996433b5a2e33bdb0b9b
KeziaWahome <k.wahome@alustudent.com>
2023-11-28 09:20:35 +0200

    update: factorial computation using memoization vs tabulation technique

--- Dynamic_Programming/Computing factorial/factorial_computation.py
+++ Dynamic_Programming/Computing factorial/factorial_computation.py
@@ -1,59 +1,62 @@
-'''1. Implement factorial computation using memoization. Test the program on n such that 4<= n < 1,000,00 
-
-2. Implement factorial computation using tabulation technique (bottom up).  Test the program on n such that 4 <= n < 1,000,000
-
-3. Identify the algorithm that works the best as the number gets big.'''
+import sys
+import timeit
 
 def factorial_memo(n, memo={}):
-    # Check if the result for n is already in the memoization table
+    """
+    Compute the factorial of a number using memoization.
+
+    Parameters:
+    - n: The input number.
+    - memo: A dictionary to store memoized results.
+
+    Returns:
+    The factorial of the input number.
+    """
     if n in memo:
         return memo[n]
-    
-    # Base case: factorial of 0 and 1 is 1
     if n == 0 or n == 1:
         return 1
-    
-    # Recursive case: calculate factorial and store result in memo table
     result = n * factorial_memo(n - 1, memo)
     memo[n] = result
     return result
 
 def factorial_tabulation(n):
-    # Create a table to store solutions to subproblems
+    """
+    Compute the factorial of a number using tabulation (bottom-up).
+
+    Parameters:
+    - n: The input number.
+
+    Returns:
+    The factorial of the input number.
+    """
     table = [0] * (n + 1)
     table[0] = 1
-    
-    # Fill in the table iteratively
     for i in range(1, n + 1):
         table[i] = i * table[i - 1]
-    
-    # Return the final result
     return table[n]
 
-def main():
-    # Test memoization
+def test_performance():
+    n_values = [10, 100, 1000, 10000 , 10**5 , 10**6, 10**7 , 10**8]  # Adjust these values as needed
+
     print("Memoization:")
-    for n in range(4, 10):
-        print(f"Factorial of {n}: {factorial_memo(n)}")
-
-    # Test tabulation
-    print("\nTabulation:")
-    for n in range(4, 10):
-        print(f"Factorial of {n}: {factorial_tabulation(n)}")
-
-    # Test performance for larger values of n
-    n_values = [10**5, 10**6, 10**7]
     for n in n_values:
-        start_time_memo = time.time()
-        _ = factorial_memo(n)
-        end_time_memo = time.time()
-        print(f"Memoization - n = {n}, Time taken: {end_time_memo - start_time_memo:.6f} seconds")
+        try:
+            sys.setrecursionlimit(10**6)  # Set a higher recursion limit
+            time_taken = timeit.timeit(lambda: factorial_memo(n), number=1000) / 1000
+            print(f"n = {n}, Time taken: {time_taken:.9f} seconds")
+        except RecursionError as e:
+            print(f"Memoization - n = {n}, RecursionError: {e}")
 
-        start_time_tabulation = time.time()
-        _ = factorial_tabulation(n)
-        end_time_tabulation = time.time()
-        print(f"Tabulation - n = {n}, Time taken: {end_time_tabulation - start_time_tabulation:.6f} seconds")
+    print("\nTabulation:")
+    for n in n_values:
+        try:
+            start_time = timeit.default_timer()
+            factorial_tabulation(n)
+            time_taken = timeit.default_timer() - start_time
+            print(f"n = {n}, Time taken: {time_taken:.9f} seconds")
+        except Exception as e:
+            print(f"Tabulation - n = {n}, Error: {e}")
 
 if __name__ == "__main__":
-    import time
-    main()
+    test_performance()
