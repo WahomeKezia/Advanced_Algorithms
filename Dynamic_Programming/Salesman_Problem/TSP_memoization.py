89fbe8b13bcb824116d03543ee6a9ee07d342ced
KeziaWahome <k.wahome@alustudent.com>
2023-11-28 09:58:14 +0200

    update:Traveling Salesman Problem using memoization

--- Dynamic_Programming/Salesman_Problem/TSP_memoization.py
+++ Dynamic_Programming/Salesman_Problem/TSP_memoization.py
@@ -1,6 +1,18 @@
 import sys
 
-def tsp(graph, current, visited, memo):
+def tsp_memoization(graph, current, visited, memo):
+    """
+    Recursive function to calculate the shortest path in the Traveling Salesman Problem using memoization.
+
+    Parameters:
+    - graph: The adjacency matrix representing the weighted graph.
+    - current: The current location (vertex) in the path.
+    - visited: A list indicating which vertices have been visited.
+    - memo: A memoization dictionary to store previously computed results.
+
+    Returns:
+    The shortest distance for the TSP.
+    """
     if tuple(visited) in memo:
         return memo[tuple(visited)]
 
@@ -12,28 +24,40 @@ def tsp(graph, current, visited, memo):
     for i in range(len(graph)):
         if not visited[i] and graph[current][i] != 0:
             visited[i] = True
-            distance = graph[current][i] + tsp(graph, i, visited, memo)
+            distance = graph[current][i] + tsp_memoization(graph, i, visited, memo)
             min_distance = min(min_distance, distance)
             visited[i] = False
 
     memo[tuple(visited)] = min_distance
     return min_distance
 
-def shortest_path(graph):
+def shortest_path_memoization(graph, vertices):
+    """
+    Function to initiate the Traveling Salesman Problem calculation using memoization.
+
+    Parameters:
+    - graph: The adjacency matrix representing the weighted graph.
+    - vertices: The list of vertices.
+
+    Returns:
+    The shortest distance for the TSP.
+    """
     n = len(graph)
     visited = [False] * n
     visited[0] = True  # Starting from the first location
     memo = {}
-    return tsp(graph, 0, visited, memo)
+    return tsp_memoization(graph, 0, visited, memo)
 
-#Graph 
+# Specify the vertices and edge weights
+vertices = ['A', 'B', 'C', 'D', 'E']
 graph = [
-    [0, 2, 9, 10],
-    [1, 0, 6, 4],
-    [15, 7, 0, 8],
-    [6, 3, 12, 0]
+    [0, 12, 10, 19, 8],
+    [6, 0, 3, 7, 6],
+    [15, 7, 0, 2, 20],
+    [19, 3, 2, 0, 4],
+    [8, 6, 20, 4, 0]
 ]
 
 # Test the program
-shortest_distance = shortest_path(graph)
+shortest_distance = shortest_path_memoization(graph, vertices)
 print(f"The shortest distance is: {shortest_distance}")
